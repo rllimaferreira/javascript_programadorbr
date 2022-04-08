edd4439da5d2bb9d08b65163afa78ecebe3a9fec
Rodrigo L. de Lima Ferreira <rodrigollferreira21@gmail.com>
2022-04-08 16:27:31 -0300

    Prática da aula 8

---
+++ javascript_basico/aula08/script.js
@@ -0,0 +1,56 @@
+// Operadores Comparativos
+
+// == igual a
+// === valor igual e igual
+// ! = não é igual
+// !== não igual ou não igual
+// > maior que
+// < menor que
+// >= maior que ou igual a
+// <= menor ou igual a
+
+// Exemplo 1
+
+console.log(2 == 3);
+
+// Exemplo 2
+
+var a = 3;
+var b = "3";
+
+console.log(a === b); // 3 e 3 é igual, mas o tipo é diferente, então é "false"
+
+// Exemplo 3
+
+var a = 3;
+var b = "3";
+
+console.log(a !== b);
+
+// Exemplo 4
+
+var a = 5;
+var b = 5;
+
+console.log(a > b);
+
+// Exemplo 5
+
+var a = 5;
+var b = "3";
+
+console.log(a <= b);
+
+// Exemplo 6
+
+var a = 3;
+var b = 4;
+
+console.log(a < b);
+
+// Exemplo 7
+
+var a = 5;
+var b = 4;
+
+console.log(a <= b);
