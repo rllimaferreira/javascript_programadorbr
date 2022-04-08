c4b9a4aee123ff9ffddcb1f1bb457dc3c406e760
Rodrigo L. de Lima Ferreira <rodrigollferreira21@gmail.com>
2022-04-07 22:26:17 -0300

    Prática da aula 4

---
+++ javascript_basico/aula04/script.js
@@ -0,0 +1,24 @@
+// var a = "Placa de Captura";
+
+// Ou
+
+// var a
+
+// a = "Hd";
+
+// a = "Placa de captura"
+
+// console.log(a);
+
+// var caixaGrande 
+
+// A ordem é muito importante. No exemplo abaixo, o resultado de b passa a ser indefinido.
+
+var a;
+
+var b = "Hd";
+b = a;
+a = "Placa de captura";
+
+console.log(b);
+
