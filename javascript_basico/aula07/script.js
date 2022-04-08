3dddad2ee256eb459f83fa4c7701b7c4508b8536
Rodrigo L. de Lima Ferreira <rodrigollferreira21@gmail.com>
2022-04-08 16:07:53 -0300

    Prática da aula 7

---
+++ javascript_basico/aula07/script.js
@@ -0,0 +1,50 @@
+// =     x = y     x = y
+// +=    x += y    x = x + y
+// -=    x -= y    x = x - y
+// *=    x *= y    x = x * y
+// /=    x /= y    x = x / y
+// %=    x %= y    x = x % y
+
+// Exemplo 1
+
+var a = 30;
+
+a += 1; // É o mesmo que a = a + 5
+
+console.log(a);
+
+// Exemplo 2
+
+var a = 30;
+
+a *= 2;
+
+console.log(a);
+
+// Exemplo 3
+
+var a = 30;
+
+a /= 2;
+
+console.log(a);
+
+// Exemplo 4
+
+var a = 30;
+
+a %= 28;
+
+console.log(a);
+
+// Exemplos que desenvolvi ao longo da aula
+
+a += 1; // É o mesmo que a = a + 1
+
+a -= 1; // É o mesmo que a = a - 1
+
+a *= 1 // É o mesmo que a = a * 1
+
+a /= 1; // É o mesmo que a = a / 1
+
+a %= 1; // É o mesmo que a = a % 1
