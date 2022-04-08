3c964be0d48121fb677b4bf7a5304bfccce77230
Rodrigo L. de Lima Ferreira <rodrigollferreira21@gmail.com>
2022-04-08 15:38:36 -0300

    Prática da aula 6

---
+++ javascript_basico/aula06/script.js
@@ -0,0 +1,54 @@
+// Operadores Aritméticos
+
+// + Adição
+// - Subtração
+// * Multiplicação
+// / Divisão
+// % Módulo
+// ++ Incremento
+// -- Decremento
+
+// Exemplo 1
+
+var a = 10;
+var b = 15;
+
+var c = a / b;
+
+console.log(c);
+
+// Exemplo 2
+
+var a = 10;
+var b = 15;
+
+var c = a % b;
+
+console.log(c);
+
+// Exemplo 3
+
+var a = 30;
+
+a++; // É o mesmo que a = a + 1
+
+console.log(a); // Imprime 30
+
+// Exemplo 4
+
+var a = 30;
+
+var b = a--;
+
+console.log(b); // Imprime 30
+console.log(a); // Imprime 29 - Houve a decrementação do a, mas só depois
+
+// Exemplo 5
+
+var a = 30;
+
+var b = --a;
+
+console.log(b); // Imprime 29
+
+// Importante - A ordem importa bastante na programação
