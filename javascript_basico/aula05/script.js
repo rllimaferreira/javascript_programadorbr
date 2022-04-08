63c111dca9d1e5abe3ba60dec7ace96a44442bd4
Rodrigo L. de Lima Ferreira <rodrigollferreira21@gmail.com>
2022-04-07 22:43:09 -0300

    Prática da aula 5

---
+++ javascript_basico/aula05/script.js
@@ -0,0 +1,47 @@
+// Boolean
+// Null
+// Undefined
+// Number
+// String - É uma cadeia de caracteres
+
+// Tudo que estiver nesta linha vai ser um comentário
+
+/*
+
+Tudo que estiver aqui dentro,
+vai ser um comentário
+*/
+
+// Exemplo 1
+var a = " Primeira linha \n Segunda Linha ";
+
+console.log(a);
+
+// Exemplo 2
+
+var a = 1;
+var b = "1";
+
+console.log(a);
+console.log(b);
+
+console.log("1" + "1"); // String - o resultado vai ser 11, porque existe a concatenação de 1 com 1 por envolver o tipo string
+console.log(1 + 1);  // Number - o resultado vai ser 2, porque existe a soma entre 1 mais 1 por envolver o tipo number
+
+// Exemplo 3
+
+var a;
+
+console.log(a);
+
+
+// Exemplo 4
+
+var a = null;
+console.log(a);
+
+// Exemplo 5
+
+var a = false;
+
+console.log(a);
