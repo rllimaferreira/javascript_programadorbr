78f4b2c7a4c3b76f37b577053fd9d9d76abe74b2
Rodrigo L. de Lima Ferreira <rodrigollferreira21@gmail.com>
2022-04-09 19:27:53 -0300

    Prática da aula 12

---
+++ javascript_basico/aula12/script.js
@@ -0,0 +1,41 @@
+// Estruturas condicionais - Parte 2
+
+// Exemplo 1
+
+var a = 3;
+
+var b = 6;
+
+var c = a + b;
+
+console.log(c);
+
+// Exemplo 2
+
+var a = "3";
+
+var b = "6";
+
+var c = a + b;
+
+console.log(c);
+
+// Exemplo 3
+
+var a = "3";
+
+var b = "6";
+
+var c = parseInt(a) + parseInt(b); // O ParseInt serve para transformar uma string em um número inteiro
+
+console.log(c);
+
+// Exemplo 4
+
+var a = "3.5"; // É um float
+
+var b = "6";
+
+var c = parseFloat(a) + parseFloat(b); // O ParseFloat serve para transformar uma string em um número com casas decimais (número Real)
+
+console.log(c);
