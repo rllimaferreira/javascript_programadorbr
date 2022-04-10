c2cb4341ec2980c70e870037735eb66026e298cf
Rodrigo L. de Lima Ferreira <rodrigollferreira21@gmail.com>
2022-04-09 22:32:07 -0300

    Prática da aula 13

---
+++ javascript_basico/aula13/script.js
@@ -0,0 +1,17 @@
+// Alert e Prompt
+
+// alert("Bom dia, Rodrigo!");
+
+// Exemplo 1
+
+var nome = prompt("Qual é o seu nome?");
+
+alert("Bom dia, " + nome);
+
+// Exemplo 2
+
+var numero = parseInt(prompt("Digite um número"));
+
+var dobro = numero + numero;
+
+alert("O dobro de " + numero + " é " + dobro);
